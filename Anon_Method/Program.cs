d1bbbec87ecaa9c253bdbcb013161198b430111f
Мурад Рагимов <ragimovu1@tk34.mskobr.ru>
2022-04-11 10:28:04 +0300

    Добавьте файлы проекта.

---
+++ Anon_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Anon_Method
+{
+    public delegate void MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate mD = new MyDelegate(delegate { Console.WriteLine("Hello world!"); });
+            mD();
+        }
+    }
+}
---
+++ Anon_Method1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Anon_Method1
+{
+    public delegate int AddDelegate(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddDelegate aD = new AddDelegate(delegate (int number1, int number2)
+            {
+                int res = number1 + number2;
+                return res;
+            });
+            int sum = aD(5, 4);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
